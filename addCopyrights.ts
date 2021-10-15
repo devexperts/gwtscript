5434f54b358e5ecc37e2aad63adeea92bc49f86d
Evgeny Nikiforov <enikiforov@devexperts.com>
2021-10-15 21:42:42 +0300

    [IO-416] Add license agreement//before test

--- addCopyrights.ts
+++ addCopyrights.ts
@@ -31,7 +31,7 @@ const run = async (filePattern: string, ignoreFiles: string) => {
         });
 
         if (content.search(COPYRIGHT_TEXT) === -1) {
-            await file.write(contentToBuffer, 0);
+            await file.write(contentToBuffer, 0, contentToBuffer.length, 0);
         }
         file.close();
     }
