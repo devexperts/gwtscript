bb6ed8417ac4d67bec872e00fb29dffbcc833c91
Evgeny Nikiforov <enikiforov@devexperts.com>
2022-01-25 12:40:08 +0300

    PR fixes

--- src/utils/joinLeft.ts
+++ src/utils/joinLeft.ts
@@ -5,15 +5,11 @@ License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at https://mozilla.org/MPL/2.0/.
 */
 
-import { Either, isRight, left } from "fp-ts/lib/Either";
+import { Either, getOrElseW, mapLeft } from "fp-ts/lib/Either";
+import { pipe } from "fp-ts/lib/pipeable";
 
 export const joinLeft = <LL, LR, R>(
     either: Either<Either<LL, LR>, R>
 ): Either<LL | LR, R> => {
-    if (isRight(either)) return either;
-    const leftEither = either.left;
-
-    if (isRight(leftEither)) return left(leftEither.right);
-
-    return leftEither;
+    return pipe(either, mapLeft(getOrElseW((err) => err)));
 };
--- src/utils/sequenceReaderEither.ts
+++ src/utils/sequenceReaderEither.ts
@@ -5,28 +5,19 @@ License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at https://mozilla.org/MPL/2.0/.
 */
 
-import { left, right } from "fp-ts/lib/Either";
+import { isEmpty, partitionMap } from "fp-ts/lib/Array";
+import { fromPredicate } from "fp-ts/lib/Either";
+import { constant } from "fp-ts/lib/function";
+import { pipe } from "fp-ts/lib/pipeable";
 import { ReaderEither } from "fp-ts/lib/ReaderEither";
 
 export const sequenceReaderEither = <Env, Left, Right>(
     items: ReaderEither<Env, Left, Right>[]
 ): ReaderEither<Env, Left[], Right[]> => (env) => {
-    const errors: Left[] = [];
-    const values: Right[] = [];
-
-    items.forEach((reader) => {
-        const either = reader(env);
-
-        if (either._tag === "Left") {
-            errors.push(either.left);
-        } else {
-            values.push(either.right);
-        }
-    });
-
-    if (errors.length > 0) {
-        return left(errors);
-    }
-
-    return right(values);
+    return pipe(
+        items,
+        partitionMap((r) => r(env)),
+        ({ left, right }) =>
+            pipe(right, fromPredicate(constant(isEmpty(left)), constant(left)))
+    );
 };
